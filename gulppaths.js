e76a8c2b018536d5d5812c379820d6111dc22da8
Lucas Di Mattia <lucasdimattia@gmail.com>
2021-08-14 23:43:09 -0300

    Remove unneccesary paths

--- gulppaths.js
+++ gulppaths.js
@@ -5,53 +5,31 @@ module.exports = {
     htaccess: 'src/.htaccess',
     scripts: 'src/assets/js/**/*.js',
     styles: 'src/assets/scss/**/*.scss',
-    fonts: 'src/assets/fonts/**/*',
+    fonts: 'src/assets/fonts/**/*.{otf,ttf,svg,eot,woff,woff2}',
     images: 'src/assets/images/**/*.{jpg,png,gif,jpeg,svg}',
     docs: 'src/assets/docs/**/*.{pdf,xls,xlsx,doc,docx}',
-    icons: 'src/assets/icons',
-    vendor: {
-      styles: [
-        './node_modules/bootstrap-sass/assets/stylesheets',
-        './node_modules/slick-carousel/slick'
-      ],
-      scripts: [
-        './node_modules/jquery/dist/jquery.js',
-        './node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
-        './node_modules/slick-carousel/slick/slick.js',
-        './node_modules/scrollmonitor/scrollMonitor.js',
-        './node_modules/scrollmonitor-parallax/index.js',
-        './node_modules/jquery.scrollto/jquery.scrollTo.js',
-        './node_modules/gsap/src/uncompressed/TweenMax.js',
-        './node_modules/headroom.js/dist/jQuery.headroom.min.js',
-        './node_modules/headroom.js/dist/headroom.min.js',
-        './node_modules/moment/moment.js'
-      ],
-      static: [
-        './node_modules/bootstrap-sass/assets/fonts/**/*',
-        './node_modules/slick-carousel/slick/fonts/**/*'
-      ]
-    }
+    icons: 'src/assets/icons/**/*.*'
   },
 
   dev: {
     base: 'dev',
     markup: 'dev',
-    fonts: 'dev/fonts',
-    icons: 'dev/icons',
-    images: 'dev/images',
     scripts: 'dev/js',
     styles: 'dev/css',
+    fonts: 'dev/fonts',
+    images: 'dev/images',
     docs: 'dev/docs',
+    icons: 'dev'
   },
 
   dist: {
     base: 'dist',
     markup: 'dist',
-    fonts: 'dist/fonts',
-    icons: 'dist/icons',
-    images: 'dist/images',
     scripts: 'dist/js',
     styles: 'dist/css',
+    fonts: 'dist/fonts',
+    images: 'dist/images',
     docs: 'dist/docs',
+    icons: 'dist'
   }
 }
